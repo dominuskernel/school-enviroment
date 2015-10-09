9c312d7e97d1e04bc199fb0ecf9dd5fdf43e0268
dominuskernel <dominuskernel@gmail.com>
2015-10-10 01:32:40 +0200

    clear(sources): clear unless files

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,109 @@
 var gulp            = require('gulp'),
-    // this is an arbitrary object that loads all gulp plugins in package.json.
+// this is an arbitrary object that loads all gulp plugins in package.json.
     $           = require("gulp-load-plugins")(),
-    path        = require('path'),
     browserSync = require('browser-sync'),
+    coffeelint  = require("gulp-coffeelint"),
+    del         = require("del"),
     merge       = require('merge-stream'),
+    path        = require('path'),
     reload      = browserSync.reload,
+    runSequence = require('run-sequence'),
     tinylr      = require('tiny-lr'),
-    sass        = require('gulp-sass');
+    sass        = require('gulp-sass'),
     server      = tinylr();
 
 gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: "./views"
-    }
-  });
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 gulp.task('coffee', function() {
     return gulp.src('src/scripts/*.coffee')
         .pipe($.plumber())
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter())
         .pipe($.coffee({bare: true}).on('error', $.util.log))
-        .pipe(gulp.dest('views/scripts'))
+        .pipe(gulp.dest('./scripts'))
         .pipe(browserSync.reload({stream: true}))
         .pipe($.livereload( server ));
 });
 
 gulp.task('compass', function() {
-  return gulp.src('./src/stylesheets/*.scss')
-    .pipe($.plumber())
-    .pipe(browserSync.reload({stream: true}))
-    .pipe($.compass({
-      css: 'views/stylesheets',
-      sass: 'src/stylesheets'
-    }))
-    .pipe(gulp.dest('views/stylesheets'))
-    .pipe($.livereload( server ));;
-});
-
-gulp.task('components', function(){
-    return gulp.src('bower_components/**/*.*')
+    return gulp.src('./src/stylesheets/*.sass')
         .pipe($.plumber())
-        .pipe(gulp.dest('views/bower_componets'))
         .pipe(browserSync.reload({stream: true}))
-        .pipe($.livereload( server ));
+        .pipe($.compass({
+            css: './stylesheets',
+            sass: 'src/stylesheets'
+        }))
+        .pipe(gulp.dest('./stylesheets'))
+        .pipe($.livereload( server ));;
 });
 
 gulp.task('demo', function() {
-  var jadeDemo = gulp.src('demo/index.jade')
-      .pipe($.plumber())
-      .pipe($.jade({
-          pretty: true
-      }))
-      .pipe(gulp.dest('views'))
-      .pipe(browserSync.reload({stream: true}))
-      .pipe( $.livereload( server ) );
-  var scssDemo = gulp.src('demo/main.scss')
-      .pipe($.plumber())
-      .pipe($.compass({
-          css: 'views',
-          sass: 'demo'
-      }))
-      .pipe(gulp.dest('views'))
-      .pipe(browserSync.reload({stream: true}))
-      .pipe($.livereload( server ));
-  var coffeeDemo = gulp.src('demo/app.coffee')
-      .pipe($.plumber())
-      .pipe($.coffee({bare: true}).on('error', $.util.log))
-      .pipe(gulp.dest('views'))
-      .pipe(browserSync.reload({stream: true}))
-      .pipe($.livereload( server ));
+    var jadeDemo = gulp.src('demo/index.jade')
+        .pipe($.plumber())
+        .pipe($.jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
+    var sassDemo = gulp.src('demo/main.sass')
+        .pipe($.plumber())
+        .pipe($.compass({
+            css: './',
+            sass: 'demo'
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe( $.livereload( server ) );
+    var coffeeDemo = gulp.src('demo/app.coffee')
+        .pipe($.plumber())
+        .pipe(coffeelint())
+        .pipe(coffeelint.reporter())
+        .pipe($.coffee({bare: true}).on('error', $.util.log))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe( $.livereload( server ) );
+    var mocksDemo = gulp.src('demo/mocks/*.json')
+        .pipe($.plumber())
+        .pipe(gulp.dest('./mocks'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe( $.livereload( server ) );
+    return merge(jadeDemo, sassDemo, coffeeDemo, mocksDemo)
 
-    return merge(jadeDemo, scssDemo, coffeeDemo)
 });
 
 gulp.task('jade', function () {
-  return gulp.src('src/jade/*.jade')
-    .pipe($.plumber())
-    .pipe($.jade({
-      pretty: true
-    })).pipe( gulp.dest('views/templates'))
-    .pipe(browserSync.reload({stream: true}))
-    .pipe( $.livereload( server ) );
+    var testJade = gulp.src('src/jade/*.jade')
+        .pipe($.plumber())
+        .pipe($.jade({
+            pretty: true
+        })).pipe( gulp.dest('./bower_components/dmk-menu/templates/'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe( $.livereload( server ) );
+    var jadeComponent = gulp.src('src/jade/*.jade')
+        .pipe($.plumber())
+        .pipe($.jade({
+            pretty: true
+        })).pipe( gulp.dest('templates/'))
+        .pipe(browserSync.reload({stream: true}))
+        .pipe( $.livereload( server ) );
+    return merge(testJade, jadeComponent)
 });
 
-gulp.task('build', ['coffee', 'compass', 'components', 'demo', 'jade']);
+gulp.task('build', ['coffee', 'compass', 'demo', 'jade']);
 
-gulp.task('serve', ['build', 'browser-sync'], function () {
-  gulp.watch('src/scripts/*.coffee',['coffee', reload]);
-  gulp.watch('src/stylesheets/*.scss',['compass', reload]);
-  gulp.watch('src/bower_components/**/*.*',['components', reload]);
-  gulp.watch('demo/**/*.*'['demo', reload]);
-  gulp.watch('src/jade/*.jade', ['views', reload]);
+gulp.task('server', function (callback) {
+    runSequence('build','browser-sync');
+    gulp.watch('src/scripts/*.coffee',['coffee', reload]);
+    gulp.watch('src/stylesheets/*.sass',['compass', reload])
+    gulp.watch('src/stylesheets/*.sass',['demo', reload]);
+    gulp.watch('demo/**/*.*', ['demo', reload]);
+    gulp.watch('src/jade/*.jade', ['jade', reload]);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('default', ['server']);
--- views/app.js
+++
@@ -1,4 +0,0 @@
-(function() {
-  'use strict';
-  angular.module('app', []).controller('appControl', []);
-})();
